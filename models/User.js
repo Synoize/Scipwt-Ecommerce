c19ca9902b358e33c4fff1e740dad4b4830a0de2
synoize <suryavanshivam@gamil.com>
2025-08-15 00:09:11 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,14 @@
+import {Schema, model, models} from 'mongoose';
+
+const userSchema = new Schema({
+    _id: {type: String, required: true},
+    name : {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    imageUrl : {type: String, required: true},
+    cartItems: {type: Object, required: true},
+}, {minimize: false})
+
+
+const UserModel = models.user || model('user', userSchema);
+
+export default UserModel;
